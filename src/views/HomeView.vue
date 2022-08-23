ebd0001056ba7696884e7547c8c9d08e64462e04
ruchan12 <ruchan12@users.noreply.github.com>
2022-08-23 23:07:14 +0900

    add background

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,34 @@
 <template>
     <div class="common-layout">
-       <el-button type="primary">Primary</el-button>
-    <el-container>
-      <el-header>
-        Header</el-header>
-
-      <el-footer>Footer</el-footer>
+      <!-- <el-button type="primary">Primary</el-button> -->
       <el-container>
-      <el-header>Profile</el-header>
-      name　眞田将希 (Sanada Masaki)
-      <el-main>Main
-      Age 22
-       学歴</el-main>
+        <el-header class="profile">Profile</el-header>
+            <el-main>
+              name 眞田将希 (Sanada Masaki) <br>
+              Age 22<br>
+              学歴<br>
+            </el-main>
+        <el-header>スキル</el-header>
+      <el-main></el-main>
+        <el-header>各種リンク</el-header>
+            <el-main>
+              aaa
+            </el-main>
+
 </el-container>
-      <el-header>スキル</el-header>
       
-      <el-header>各種リンク</el-header>
-    
-    aaaa</el-container>
   </div>
 </template>
 
 <script lang="ts">
 
 </script>
+
+<style>
+.el-header {
+  background-color: #97cdf3;
+}
+.profile {
+  background-color: #97cdf3;
+}
+</style>
