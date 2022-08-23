86887823b25c9b7caf1e7a74931be45c0f436e4c
ruchan12 <ruchan12@users.noreply.github.com>
2022-08-23 19:06:39 +0900

    設定の編集

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="common-layout">
+       <el-button type="primary">Primary</el-button>
+    <el-container>
+      <el-header>
+        Header</el-header>
+
+      <el-footer>Footer</el-footer>
+      <el-container>
+      <el-header>Profile</el-header>
+      name　眞田将希 (Sanada Masaki)
+      <el-main>Main
+      Age 22
+       学歴</el-main>
+</el-container>
+      <el-header>スキル</el-header>
+      
+      <el-header>各種リンク</el-header>
+    
+    aaaa</el-container>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
 </script>
