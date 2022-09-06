9d203150d9dbf031e6d6979d56d834b5d6939e2b
ruchan12 <ruchan12@users.noreply.github.com>
2022-09-06 17:50:03 +0900

    レイアウト少し変更

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,35 +2,47 @@
     <div class="common-layout">
       <!-- <el-button type="primary">Primary</el-button> -->
       <el-container>
-        <el-header class="profile" style="height:30px;">Profile</el-header>
+        <el-header class="profile" style="height:30px;padding: auto;">Profile</el-header>
             <el-main>
               name 眞田将希 (Sanada Masaki) <br>
-              Age 22<br>
-              学歴<br>
-            </el-main>
-      </el-container>
+              Age 22 <br>
+              学歴 <br>
+              <el-card class="box-card">
+                <template #header>
+                  <div class="card-header">
+                    <span>スキル</span>
+                  </div>
+                </template>
+                <div> Network
+                      machineLeaning
+                      Python
+                      Git
+                      AWS
+            </div>
 
-      <el-container>
-        <el-header class="skill">スキル</el-header>
-            <el-main>
-              Network
-              machineLeaning
 
-              Python
-              Git
-              AWS
-            </el-main>
-      </el-container>
-      <el-container>
-        <el-header class="link">各種リンク</el-header>
-            <el-main>
-              note
+              </el-card>
+              <el-card class="box-card">
+                <template #header>
+                  <div class="card-header">
+                    <span>研究</span>
+                  </div>
+                </template>
+                <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+              </el-card>
+
+              <el-card class="box-card">
+                <template #header>
+                  <div class="card-header">
+                    <span>各種リンク</span>
+                  </div>
+                </template>
+                <div v-for="o in 4" :key="o" class="text item">              note
               Qiita
-              github
+              github</div>
+              </el-card>
             </el-main>
-
-</el-container>
-      
+      </el-container>
   </div>
 </template>
 
@@ -52,5 +64,22 @@
   background-color: #97cdf3;
 }
 
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+  width: 480px;
+  float: left;
+}
 </style>
