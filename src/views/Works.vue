a05f359fd2b88e733af8304c70ae267452fd57ce
masaki12-s <asam.archer1112@yahoo.ne.jp>
2022-10-25 02:26:51 +0900

    scrollbar 追加

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -7,27 +7,22 @@
             <el-col :span="12">
                 <el-card class="box-card" :body-style="{ padding: '10px' }">
                   <b>プログラミング言語</b>
-            
-                    <div> 
-                        Python <br>
-                        Java <br>
-                        C# <br>
-                        Go <br>
-                        Vue.js <br>
-                    </div>
+                  <el-scrollbar height="400px">
+                    <p v-for="item in language" :key="item" class="scrollbar-demo-item">{{ item }}</p>
+                </el-scrollbar>
+
                 </el-card>
             </el-col>
   
             <el-col :span="12">
                 <el-card class="box-card" :body-style="{ padding: '0px' }">
-                    <div class="card-header">
-                        <span>プログラミング言語</span>
-                    </div>
-                    <div> Network
-                      machineLeaning
-                      Python
-                      Git
-                      AWS
+                    <!-- <div class="card-header"> -->
+                        <b>分野</b>
+                        <el-scrollbar height="400px">
+                            <p v-for="item in field" :key="item" class="scrollbar-demo-item">{{ item }}</p>
+                        </el-scrollbar>
+                    
+                    <div> 
                     </div>
                 </el-card>
             </el-col>
@@ -36,3 +31,28 @@
     </el-main>
      
 </template>
+
+<script lang="ts">
+export default {
+    data: () => {
+        return {
+            language: ["Python3", "Java", "C#"],
+            field: ["Network", "machineLeaning", "Git", "AWS"],
+        }
+    }
+}
+    
+</script>
+<style scoped>
+.scrollbar-demo-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  margin: 10px;
+  text-align: center;
+  border-radius: 4px;
+  background: var(--el-color-primary-light-9);
+  color: var(--el-color-primary);
+}
+</style>
