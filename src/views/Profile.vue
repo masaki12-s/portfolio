1d27932466ffa0c5514d06bef57cb94d421fab28
masaki12-s <70832772+masaki12-s@users.noreply.github.com>
2022-11-09 20:51:26 +0900

    プロフィール追記

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -8,19 +8,32 @@
                 <p class="name">NAME: 眞田将希 (Sanada Masaki) </p><br>
               </el-col>
               <el-col :span="12">
-                       
-                       <p>
-                        現在<b>奈良先端科学技術大学院大学</b> 在学中。
-                        <b>ネットワーク</b>の研究室に属しており，Wi-Fi等の電波強度を用いた屋内測位に関する研究を行っております。
-                        <br>
-                       現在APIについて学習中。
-                       バックエンドエンジニアorインフラエンジニアを目指して就活中です。
+                <p class="title">自己紹介</p>
+                <p>
+                Age：22 <br>
+                趣味：PC触る、食べ物フェスいく<br>
+                目標：就活終わらせて、欲しいもの（時計、macbook）を買う、旅行行く  <br>
+                    <br>
+                    現在<b>奈良先端科学技術大学院大学</b> 在学中です！ <br>
+                    <b>ネットワーク</b>の研究室に属しており、インフラ（特にサーバ）にたくさん触れている段階です。
+                    Wi-Fi等の電波強度を用いた屋内測位に関する研究を行っております。
+                    現在APIについて学習中。Golangを初めて使って作ってます。
+                    バックエンドエンジニアorインフラエンジニアを目指して就活中です。
                       </p>
-                        <br>
-                        Age 22 <br>
-                        趣味：PC触る、食べ物フェスいく<br>
-                        目標：就活終わらせて、欲しいもの（時計、macbook）を買う<br>
-                        旅行行く  <br>
+                    <br>
+                <p class="title">スキル</p> 
+                <p>
+                ・プログラミング言語 <br>
+                <b>Python</b>, Java, C#, Javascript, C <br>
+                ・開発ツール <br>
+                GitHub, Subversion, Docker <br>
+                </p>
+                <p class="title">現在行っているバイト</p> 
+                <p>
+                <a href="https://pinebase.jp/index.php/docor_system/">Docor開発</a>
+                 <br>
+
+                </p>
               </el-col>
             </el-row>
             </el-main>
@@ -40,6 +53,11 @@
 }
 p {
   text-align: left;
+}
+.title {
+  text-align: center;
+  font-weight: bold;
+  font-size: large;
 }
 </style>
   
