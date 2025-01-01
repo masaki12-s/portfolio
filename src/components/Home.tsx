c2f7cb2cb06efc3ea55ad906e087eedf94e0057a
masaki12-s <masaki.sanada@yappli.co.jp>
2025-01-01 21:51:07 +0900

    [update] homeを再利用しやすい形に修正する

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,19 @@
-import * as React from 'react';
-import styled from '@mui/material/styles/styled';
+import '../style/Home.css';
 import Stacks from './Stacks';
 import Outputs from './Outputs';
 
 export default function Home() {
+    const contents: JSX.Element[] = [
+        <Stacks />,
+        <Outputs />
+    ];
     return (
         <div>
             <h1>Masaki Sanada</h1>
             Portal to my projects
-            <Stacks />
-            <Outputs />
+            {contents.map((contents) => (
+                <div className="Home-content">{contents}</div>
+            ))}
         </div>
     );
 }
