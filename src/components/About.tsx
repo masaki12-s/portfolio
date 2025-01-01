39f26a6d6acadc6c5ac7ff3bc04491832f74cece
masaki12-s <masaki.sanada@yappli.co.jp>
2025-01-01 18:13:07 +0900

    [add] intern

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -28,7 +28,7 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
     },
 }));
 
-function createData(
+function createEducationData(
     startdate: string,
     enddate: string,
     affiliation: string,
@@ -38,14 +38,14 @@ function createData(
 }
 
 const rows = [
-    createData('2015年 4月', '2020年 3月', '奈良工業高等専門学校', '情報工学科'),
-    createData('2020年 4月', '2022年 3月', '奈良工業高等専門学校 専攻科', '情報システムコース'),
-    createData('2022年 4月', '2024年 3月', '奈良先端科学技術大学院大学', '情報科学研究科 修士課程'),
+    createEducationData('2015年 4月', '2020年 3月', '奈良工業高等専門学校', '情報工学科'),
+    createEducationData('2020年 4月', '2022年 3月', '奈良工業高等専門学校 専攻科', '情報システムコース'),
+    createEducationData('2022年 4月', '2024年 3月', '奈良先端科学技術大学院大学', '情報科学研究科 修士課程'),
 ];
 
-export function TableData() {
+export function EducationTableData() {
     return (
-        <TableContainer sx={{ maxWidth: 4/5 }} component={Paper}>
+        <TableContainer sx={{ margin: 5, marginRight: 5 }} component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                     <TableRow>
@@ -73,12 +73,110 @@ export function TableData() {
     );
 }
 
+function createInternshipData(
+    startdate: string,
+    enddate: string,
+    affiliation: string,
+    content: string,
+    job: string
+) {
+    return { startdate, enddate, affiliation, content, job };
+}
+
+const internshiprows = [
+    createInternshipData('2022年 11月', '2022年 12月', '株式会社リクルート', 'SRE', 'KubeCostの検証'),
+    createInternshipData('2023年 3月', '2023年 3月', '株式会社ヤプリ', 'SRE', 'Sentryのコンテナ化'),
+];
+
+export function InternshipTableData() {
+    return (
+        <TableContainer sx={{ maxWidth: 1, margin: 2 }} component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell>開始</StyledTableCell>
+                        <StyledTableCell>終了</StyledTableCell>
+                        <StyledTableCell>会社名</StyledTableCell>
+                        <StyledTableCell>職種</StyledTableCell>
+                        <StyledTableCell>業務内容</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {internshiprows.map((row) => (
+                        <StyledTableRow key={row.startdate}>
+                            <StyledTableCell component="th" scope="row">
+                                {row.startdate}
+                            </StyledTableCell>
+                            <StyledTableCell>{row.enddate}</StyledTableCell>
+                            <StyledTableCell>{row.affiliation}</StyledTableCell>
+                            <StyledTableCell>{row.content}</StyledTableCell>
+                            <StyledTableCell>{row.job}</StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
+function createWorkExperienceData(
+    startdate: string,
+    enddate: string,
+    affiliation: string,
+    content: string,
+    job: string
+) {
+    return { startdate, enddate, affiliation, content, job };
+}
+
+const workrows = [
+    createWorkExperienceData('2024年 4月', '現在', '株式会社ヤプリ', 'SRE', ''),
+];
+
+export function WorkExperienceTableData() {
+    return (
+        <TableContainer sx={{ maxWidth: 1, margin: 2 }} component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell>開始</StyledTableCell>
+                        <StyledTableCell>終了</StyledTableCell>
+                        <StyledTableCell>会社名</StyledTableCell>
+                        <StyledTableCell>職種</StyledTableCell>
+                        <StyledTableCell>業務内容</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+
+                <TableBody>
+                    {workrows.map((row) => (
+                        <StyledTableRow key={row.startdate}>
+                            <StyledTableCell component="th" scope="row">
+                                {row.startdate}
+                            </StyledTableCell>
+                            <StyledTableCell>{row.enddate}</StyledTableCell>
+                            <StyledTableCell>{row.affiliation}</StyledTableCell>
+                            <StyledTableCell>{row.content}</StyledTableCell>
+                            <StyledTableCell>{row.job}</StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
+
 export default function About() {
     return (
         <div>
             <h1>Masaki Sanada</h1>
-            Portal to my projects
-            <TableData />
+            これまで
+            <h2>Education</h2>
+            <EducationTableData />
+            <h2>Internship</h2>
+            <InternshipTableData />
+            <h2>Work Experience</h2>
+            <WorkExperienceTableData />
         </div>
     );
 }
